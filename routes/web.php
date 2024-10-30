e3de8cd10fad94b54de2405cccc987d0be727037
unknown <daffamusyaffa10@gmail.com>
2024-10-30 12:55:34 +0700

    Belajar Laravel 11 | 6. View Data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,16 +11,51 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Muhammad Daffa Musyaffa', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Muhammad Daffa Musyaffa',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, placeat.'
+        ],
+
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Muhammad Daffa Musyaffa',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, placeat.'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Muhammad Daffa Musyaffa',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, placeat.'
+        ],
+
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Muhammad Daffa Musyaffa',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, placeat.'
+        ]
+    ];
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
-
-// Buat 2 rute baru
-// 1. /blog
-// 2 buah artikel, judul dan isi
-// 2. /contact
-// email / social media
